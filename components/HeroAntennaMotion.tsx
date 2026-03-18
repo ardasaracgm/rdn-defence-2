35367c8f01e099805e0388ed74b81efa5262ab5f
ardasaracgm <ardasaracgm@gmail.com>
2026-03-18 22:14:21 +0300

    Update HeroAntennaMotion.tsx

--- components/HeroAntennaMotion.tsx
+++ components/HeroAntennaMotion.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 type Props = {
   src: string;
@@ -10,25 +10,12 @@ type Props = {
 
 export default function HeroAntennaMotion({ src, alt }: Props) {
   const { scrollY } = useScroll();
-
-  // scroll'a göre dönüş
   const rotate = useTransform(scrollY, [0, 600], [0, 12]);
-
-  // hafif zoom (premium hissi)
   const scale = useTransform(scrollY, [0, 600], [1, 1.05]);
 
   return (
-    <motion.div
-      style={{ rotate, scale }}
-      className="relative h-full w-full"
-    >
-      <Image
-        src={src}
-        alt={alt}
-        fill
-        className="object-cover"
-        priority
-      />
+    <motion.div style={{ rotate, scale }} className="relative h-full w-full">
+      <Image src={src} alt={alt} fill className="object-cover" priority />
     </motion.div>
   );
 }
