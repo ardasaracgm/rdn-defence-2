e47470a9da1f9cc17a1b48b595b3b4e967c59793
ardasaracgm <ardasaracgm@gmail.com>
2026-03-21 21:02:05 +0300

    Update routing.ts

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,1 +1,7 @@
+import { defineRouting } from "next-intl/routing";
 
+export const routing = defineRouting({
+  locales: ["en", "tr", "ar", "ru"],
+  defaultLocale: "en",
+  localePrefix: "as-needed", // /en is omitted, /tr /ar /ru shown
+});
