6a2fb477206a9ccc59323fd1ea55e471358f0a9d
ardasaracgm <ardasaracgm@gmail.com>
2026-03-21 23:44:22 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,15 @@
+import { MetadataRoute } from "next";
 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/thank-you", "/api/"],
+      },
+    ],
+    sitemap: "https://www.rdnsoft.com/sitemap.xml",
+    host: "https://www.rdnsoft.com",
+  };
+}
