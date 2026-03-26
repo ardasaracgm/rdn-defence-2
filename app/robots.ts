c9abd6e1dc2e8e1103c6893f3fd21e1352a8f821
ardasaracgm <ardasaracgm@gmail.com>
2026-03-26 22:37:35 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,15 +1,31 @@
 import { MetadataRoute } from "next";
 
+const BASE_URL = "https://www.rdnsoft.com";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: "*",
         allow: "/",
+        disallow: ["/thank-you", "/api/", "/_next/", "/static/"],
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
         disallow: ["/thank-you", "/api/"],
       },
+      {
+        userAgent: "YandexBot",
+        allow: "/",
+        disallow: ["/thank-you", "/api/"],
+      },
+      {
+        userAgent: "YandexImages",
+        allow: "/",
+      },
     ],
-    sitemap: "https://www.rdnsoft.com/sitemap.xml",
-    host: "https://www.rdnsoft.com",
+    sitemap: `${BASE_URL}/sitemap.xml`,
+    host: BASE_URL,
   };
 }
